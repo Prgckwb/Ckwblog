597e27e953209c50a1709727dc6e973c5dca4438
Prgckwb <ckwb.uec@gmail.com>
2022-02-19 16:08:11 +0900

    add: Headerとその他

--- src/jsMain/kotlin/components/Layout.kt
+++ src/jsMain/kotlin/components/Layout.kt
@@ -1,2 +1,20 @@
 package components
 
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.css.*
+import org.jetbrains.compose.web.dom.Div
+
+@Composable
+fun Layout(content: @Composable () -> Unit) {
+    Div({
+        style {
+            display(DisplayStyle.Flex)
+            flexDirection(FlexDirection.Column)
+            height(100.percent)
+            margin(0.px)
+            boxSizing("border-box")
+        }
+    }) {
+        content()
+    }
+}
--- src/jsMain/kotlin/content/Header.kt
+++ src/jsMain/kotlin/content/Header.kt
@@ -1,2 +1,23 @@
 package content
 
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.web.attributes.href
+import org.jetbrains.compose.web.dom.*
+
+@Composable
+fun Header() {
+    A (
+        attrs = {
+            href("index.html")
+        }
+    ){
+        Img(src = "./images/prgckwb.png")
+    }
+
+    Nav(){
+        Ul {
+            Li { Text("Works") }
+            Li { Text("Article") }
+        }
+    }
+}
